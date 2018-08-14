bc0b5bcb98824f43c050ee1ab6ae982cd7de3345
Alejandro Cotilla <alejandro@floatleft.tv>
2018-08-14 18:42:27 -0400

    Initial Commit

---
+++ Azure iOS Facial Recognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Azure iOS Facial Recognition
+//
+//  Created by Alejandro Cotilla on 8/14/18.
+//  Copyright © 2018 Alejandro Cotilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
